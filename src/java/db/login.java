a6bc9b2bec6345627f493310b7f9172477d4d66c
Deren Vural <35734401+derenv@users.noreply.github.com>
2019-01-22 20:16:44 +0000

    Add files via upload
    update for servlet code transplanted to method in login.class
    also removed unused imports in conn.java

--- src/java/db/login.java
+++ src/java/db/login.java
@@ -25,6 +25,28 @@ public class login {
         c = conn.connect();
     }
 
+    /**
+    *
+    * @author deren vural
+    */
+    public String check_valid_details(String username, String password){
+        try {
+            //prepare statement
+            PreparedStatement ps = c.prepareStatement("SELECT UserID FROM users where Username=? and Password=?");
+            
+            ps.setString(1, username);
+            ps.setString(2, password);
+            
+            ResultSet rs = ps.executeQuery();
+            
+            //return value
+            return rs.getString("UserID");
+        }catch(SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+    
     public boolean login() throws SQLException {
         PreparedStatement pst = c.prepareStatement("Select UIsername from teachers");
         ResultSet rs = pst.executeQuery();
