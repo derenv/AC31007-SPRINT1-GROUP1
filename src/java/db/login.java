967786b4a53535695032d22e4e6e4e86f0ccdf51
lokonu <derenv@live.co.uk>
2019-01-23 12:57:04 +0000

    fix

--- src/java/db/login.java
+++ src/java/db/login.java
@@ -37,7 +37,7 @@ public class login {
     */
     public String check_valid_details(String username, String password){
         try {
-	    if(c==null){out.println("NULL CONNECTION");}
+	    if(c==null){System.out.println("NULL CONNECTION");}
             //prepare statement
             PreparedStatement ps = c.prepareStatement("SELECT UserID FROM users where Username=? and Password=?");
             
@@ -45,7 +45,7 @@ public class login {
             ps.setString(2, password);
             
             ResultSet rs = ps.executeQuery();
-	    out.println(rs);
+	    System.out.println(rs);
             
             //return value
             return rs.getString("UserID");
