b136a2f7aa59ea0615642c7c6d428124629a94da
lokonu <derenv@live.co.uk>
2019-01-24 18:17:27 +0000

    login & conn unit tests

--- test/db/loginTest.java
+++ test/db/loginTest.java
@@ -5,12 +5,7 @@
  */
 package db;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
