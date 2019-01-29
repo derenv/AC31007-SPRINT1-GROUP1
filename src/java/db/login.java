c07442b87f81134e67c4dce712330764b3a7b1ff
lokonu <derenv@live.co.uk>
2019-01-29 16:29:16 +0000

    error fix

--- src/java/db/login.java
+++ src/java/db/login.java
@@ -24,8 +24,9 @@ public class login {
         //prepare & run statement
         Object[] params = {username,password};
         data_access accessor = new data_access();
-        ResultSet rs = accessor.run_statement("SELECT UserID FROM users where Username='?' and Password='?'",params);
-        accessor.close();
+        //ResultSet rs = accessor.run_statement("SELECT UserID FROM users where Username='?' and Password='?'",params);
+        ResultSet rs = accessor.run_statement("SELECT UserID FROM users where Username='"+username+"' and Password='"+password+"'");
+        accessor.close();   
         
         //parse set of results
         if(rs == null){
